a255d06d1a9933b4f6169e4f7c6dbbe43e238159
Christine Merkel <90553560+christine-berlin@users.noreply.github.com>
2026-08-05 13:42:13 +0200

    Update kanji.js

--- kanji.js
+++ kanji.js
@@ -1,1 +1,94 @@
 
+const kanji = [
+{
+    kanji: "日",
+    meaning: "day, sun",
+    onyomi: "ニチ, ジツ",
+    kunyomi: "ひ, -び, -か",
+    examples: [
+        { word: "日本", reading: "にほん", meaning: "Japan" },
+        { word: "休日", reading: "きゅうじつ", meaning: "holiday" }
+    ]
+},
+{
+    kanji: "一",
+    meaning: "one",
+    onyomi: "イチ, イツ",
+    kunyomi: "ひと",
+    examples: [
+        { word: "一人", reading: "ひとり", meaning: "one person" }
+    ]
+},
+{
+    kanji: "国",
+    meaning: "country",
+    onyomi: "コク",
+    kunyomi: "くに",
+    examples: [
+        { word: "外国", reading: "がいこく", meaning: "foreign country" }
+    ]
+},
+{
+    kanji: "人",
+    meaning: "person",
+    onyomi: "ジン, ニン",
+    kunyomi: "ひと",
+    examples: [
+        { word: "日本人", reading: "にほんじん", meaning: "Japanese person" }
+    ]
+},
+{
+    kanji: "年",
+    meaning: "year",
+    onyomi: "ネン",
+    kunyomi: "とし",
+    examples: [
+        { word: "今年", reading: "ことし", meaning: "this year" }
+    ]
+},
+{
+    kanji: "大",
+    meaning: "big",
+    onyomi: "ダイ, タイ",
+    kunyomi: "おお",
+    examples: [
+        { word: "大学", reading: "だいがく", meaning: "university" }
+    ]
+},
+{
+    kanji: "十",
+    meaning: "ten",
+    onyomi: "ジュウ",
+    kunyomi: "とお",
+    examples: [
+        { word: "十円", reading: "じゅうえん", meaning: "ten yen" }
+    ]
+},
+{
+    kanji: "二",
+    meaning: "two",
+    onyomi: "ニ",
+    kunyomi: "ふた",
+    examples: [
+        { word: "二人", reading: "ふたり", meaning: "two people" }
+    ]
+},
+{
+    kanji: "本",
+    meaning: "book, origin",
+    onyomi: "ホン",
+    kunyomi: "もと",
+    examples: [
+        { word: "本屋", reading: "ほんや", meaning: "bookstore" }
+    ]
+},
+{
+    kanji: "中",
+    meaning: "middle, inside",
+    onyomi: "チュウ",
+    kunyomi: "なか",
+    examples: [
+        { word: "中国", reading: "ちゅうごく", meaning: "China" }
+    ]
+}
+];
